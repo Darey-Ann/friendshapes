6507fea970287742aa409fe60177b6d0d507abd7
darey <darey-ann@hotmail.com>
2025-02-14 13:50:54 +0900

    tried CDNs again

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 //import * as THREE from "./node_modules/three/build/three.module.min.js";
 //import * as THREE from 'three';
-import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.146.0/build/three.module.js';
+//import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.146.0/build/three.module.js';
+import * as THREE from 'https://unpkg.com/three@0.146.0/build/three.module.js'
 
 //import { GUI } from "./node_modules/lil-gui/dist/lil-gui.esm.js";
 //import { GUI } from 'lil-gui';
