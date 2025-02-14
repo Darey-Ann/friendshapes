bd2ef427a814ab45414491d6b2928dae7e0bcf63
darey <darey-ann@hotmail.com>
2025-02-14 11:42:36 +0900

    Fixed Vite bundler

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ export default defineConfig({
   base: '/friendshapes/', // Ensures correct paths for GitHub Pages
   build: {
     rollupOptions: {
-      external: [] // Ensures everything (including three.js) is bundled properly
+      external: ['three'] // Ensures everything (including three.js) is bundled properly
     }
   }
 });
