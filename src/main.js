b32cf5665eff77cef3d7191f5aa43a4921e15ad5
darey <darey-ann@hotmail.com>
2025-02-14 12:17:26 +0900

    redo

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import * as THREE from '../node_modules/three/build/three.module.js';
+import * as THREE from "node_modules/three/build/three.module.min.js";
 import { GUI } from 'lil-gui';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import { STLExporter } from 'three/examples/jsm/exporters/STLExporter.js';
